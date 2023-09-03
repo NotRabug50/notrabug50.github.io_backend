03da8ec703baa22bbe68c8fbbdfca462e044c699
Rabug50 <smallhacker709@gmail.com>
2023-09-03 14:53:16 +0300

    s

--- main.py
+++ main.py
@@ -15,15 +15,17 @@ args = parser.parse_args()
 
 # Configure Flask session and secret key
 app.config['SESSION_TYPE'] = 'filesystem'
-app.secret_key = 'your_secret_key'
+app.secret_key = app.config["FLASK_SECRET_KEY"]
 Session(app)
 
+# ...
+
 if __name__ == "__main__":
-    # Use the MongoDB URL passed as a command-line argument
-    mongodb_url = args.mongodb_url
+    mongodb_url = app.config['MONGODB_SERVER_URL']
 
     app.register_blueprint(login.login_bp)
     app.register_blueprint(blog.blog_bp)
 
     # Start the Flask app using Gunicorn
-    app.run(host='0.0.0.0', port=8000)
+    app.run()
+
